c040962eba5c878bd38f0db537e1835309b9c0aa
Jose Pablo Méndez González <121990707+jmendezg26@users.noreply.github.com>
2024-11-19 14:46:34 -0600

    Movimiento de ObtenerTiendas a CatalogosController

--- Controllers/VentasController.cs
+++ Controllers/VentasController.cs
@@ -15,33 +15,6 @@ namespace Borboletas.Controllers
 
         #region Metodos Obtener
 
-        [HttpGet("ObtenerTiendas")]
-        public IActionResult ObtenerTiendas()
-        {
-            List<Tiendas> ListaTiendas = new List<Tiendas>();
-            try
-            {
-                ListaTiendas = _TiendasLN.ObtenerTiendas();
-
-                if (ListaTiendas.Count > 0)
-                {
-                    return StatusCode(StatusCodes.Status200OK, JsonConvert.SerializeObject(new { msg = ListaTiendas, success = true }));
-                }
-                else if (ListaTiendas.Count == 0)
-                {
-                    return StatusCode(StatusCodes.Status200OK, JsonConvert.SerializeObject(new { msg = "No hay tiendas registrados", success = true }));
-                }
-                else
-                {
-                    return StatusCode(StatusCodes.Status200OK, JsonConvert.SerializeObject(new { msg = "No se pudo obtener la lista de clientes", success = false }));
-                }
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { msg = "Imposible ejecutar su transación", success = false });
-            }
-        }
-
         [HttpGet("ObtenerVentas")]
         public IActionResult ObtenerVentas()
         {
